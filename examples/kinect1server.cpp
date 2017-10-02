c62db9018c1197d8e7abc9f453bb4f735e448474
llandolfi <lor.landolfi@gmail.com>
2017-10-02 12:26:07 +0200

    capturing depth

--- examples/kinect1server.cpp
+++ examples/kinect1server.cpp
@@ -27,19 +27,39 @@ int main( int argc, char** argv )
 
   			if(count % 2 == 0)
   			{
-  				std::cout << "Reading " << std::endl;
-  				std::cout << RGB.cols  << " " << RGB.rows << std::endl;
-  				//std::cout << depth.cols << " " << depth.rows << std::endl;
+  				std::cout << "Case 1" << std::endl;
+  
+				cv::namedWindow("RGB", CV_WINDOW_AUTOSIZE);
+  				cv::imshow("RGB", RGB);
 
-  				cv::namedWindow("Verification", CV_WINDOW_AUTOSIZE);
-  				cv::imshow("Verification", RGB);
+  				double min,max;
+
+  				cv::minMaxIdx(depth, &min, &max);
+  				cv::Mat adjdepth;
+  				cv::convertScaleAbs(depth, adjdepth, 255/ max);
+
+  				cv::namedWindow("Depth", CV_WINDOW_AUTOSIZE);
+  				cv::imshow("Depth", adjdepth);
+
   	
-  				int k = cvWaitKey(2);
+  				int k = cvWaitKey(0);
   				if (k == 27)
   				{
   	    			to_stop = true;
   				}
 			}
+			else
+			{	
+				std::cout << "Case 2" << std::endl;
+			}
+
+			std::cout << "Reading RGB" << std::endl;
+			std::cout << RGB.cols  << " " << RGB.rows << std::endl;
+			//std::cout << depth.cols << " " << depth.rows << std::endl;
+
+			std::cout << "Reading depth" << std::endl;
+			std::cout << depth.cols << " " << depth.rows << std::endl;
+
 		}
 
 		count ++;
